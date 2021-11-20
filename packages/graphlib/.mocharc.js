2f44aedcffde582b9043f3f471af361107a12f3f
Brian Crowell <brian@fluggo.com>
2021-11-20 06:45:06 -0600

    Move test/setup.ts to root

--- packages/graphlib/.mocharc.js
+++ packages/graphlib/.mocharc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  require: ['ts-node/register', 'test/setup.ts'],
+  require: ['ts-node/register', '../../test/setup.ts'],
   spec: ['lib/**/*.spec.ts'],
   ignore: ['node_modules/**'],
   'watch-files': ['**/*.ts'],
